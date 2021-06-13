09e2ab88d0745ad0abb76785ef92cc66e3e4c759
Mihir Chauhan <39073758+mihir-chauhan@users.noreply.github.com>
2021-06-13 13:17:57 -0700

    Working Code based movement

---
+++ unityInstanceHandler.js
@@ -0,0 +1,27 @@
+UnityInstance = null;
+function check() {
+    if (UnityInstance != null) {
+        setTimeout(writeMotorPowers, 500);
+    } else {
+        setTimeout(check, 500);
+    }
+}
+
+check();
+
+function writeMotorPowers() {
+    var motorPowers = JSON.parse(localStorage.getItem('motorPowers'));
+    var motor1 = motorPowers[Object.keys(motorPowers)[0]];
+    var motor2 = motorPowers[Object.keys(motorPowers)[1]];
+    var motor3 = motorPowers[Object.keys(motorPowers)[2]];
+    var motor4 = motorPowers[Object.keys(motorPowers)[3]];
+    // var motor5 = motorPowers[Object.keys(motorPowers)[0]];
+    // var motor6 = motorPowers[Object.keys(motorPowers)[0]];
+    // var motor7 = motorPowers[Object.keys(motorPowers)[0]];
+    // var motor8 = motorPowers[Object.keys(motorPowers)[0]];
+    UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "setFrontLeftVel", motor1);
+    UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "setFrontRightVel", motor2);
+    UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "setBackLeftVel", motor3);
+    UnityInstance.SendMessage("PhotonNetworkPlayer(Clone)", "setBackRightVel", motor4);
+    check();
+}
